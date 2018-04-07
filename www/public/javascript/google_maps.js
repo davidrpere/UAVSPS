6abca5b312d2bd3e5ea6a81e6cb3f31c767889c4
Ruben Perez Vaz <rubenperezvaz@MacBook-Pro-de-Ruben.local>
2018-04-07 03:03:26 +0200

    update dashboard

--- www/public/javascript/google_maps.js
+++ www/public/javascript/google_maps.js
@@ -1,4 +1,5 @@
-var rectangle;
+var rectPoly;
+var mapa;
 
 function initMap() {
   /*var map;
@@ -22,7 +23,7 @@ function initMap() {
         fullscreenControl: false
       });
 
-      //map.setCenter(pos);
+      map.setCenter(pos);
     }, function() {
       map = new google.maps.Map(document.getElementById('map'), {
         zoom: 18,
@@ -50,7 +51,7 @@ function initMap() {
       fullscreenControl: false
     });
   }*/
-  var map = new google.maps.Map(document.getElementById('map'), {
+  mapa = new google.maps.Map(document.getElementById('map'), {
     zoom: 18,
     center: {
       lat: 42.1694839,
@@ -62,40 +63,81 @@ function initMap() {
     fullscreenControl: false,
     heading: 90,
     tilt: 45
-
   });
 
+}
+
+function add_area(){
   //Define the LatLng coordinates for the rectangle.
   var bounds = {
     north: 42.1694839,
-    south: 42.1734839,
+    south: 42.1634839,
     east: -8.683478499999978,
     west: -8.689478499999978
   };
 
   // Construct the polygon.
-  rectangle = new google.maps.Rectangle({
+  var rectangle = new google.maps.Rectangle({
     bounds: bounds,
     strokeColor: '#FF0000',
     strokeOpacity: 0.8,
     strokeWeight: 2,
     fillColor: '#FF0000',
     fillOpacity: 0.35,
+    map: mapa,
     editable: true,
     draggable: true
   });
-  rectangle.setMap(map);
+
+  rectPoly = createPolygonFromRectangle(rectangle);
 }
 
-function sendCoords() {
-  var bounds = rectangle.getBounds();
-  var northEast = bounds.getNorthEast();
-  var southWest = bounds.getSouthWest();
-  var northWest = new google.maps.LatLng(northEast.lat(), southWest.lng());
-  var southEast = new google.maps.LatLng(southWest.lat(), northEast.lng());
+function remove_area(){
+  rectPoly.setMap(null)
+}
 
-  console.log("northWest (lat/long): " + northWest);
-  console.log("northEast (lat/long): " + northEast);
-  console.log("southWest (lat/long): " + southWest);
-  console.log("southEast (lat/long): " + southEast);
+function createPolygonFromRectangle(rectangle) {
+    var map = rectangle.getMap();
+
+    var coords = [
+      { lat: rectangle.getBounds().getNorthEast().lat(), lng: rectangle.getBounds().getNorthEast().lng() },
+      { lat: rectangle.getBounds().getNorthEast().lat(), lng: rectangle.getBounds().getSouthWest().lng() },
+      { lat: rectangle.getBounds().getSouthWest().lat(), lng: rectangle.getBounds().getSouthWest().lng() },
+      { lat: rectangle.getBounds().getSouthWest().lat(), lng: rectangle.getBounds().getNorthEast().lng() }
+    ];
+
+    // Construct the polygon.
+    var rectPoly = new google.maps.Polygon({
+        path: coords,
+        editable: true,
+        draggable: true
+    });
+    var properties = ["strokeColor","strokeOpacity","strokeWeight","fillOpacity","fillColor"];
+    //inherit rectangle properties
+    var options = {};
+    properties.forEach(function(property) {
+        if (rectangle.hasOwnProperty(property)) {
+            options[property] = rectangle[property];
+        }
+    });
+    rectPoly.setOptions(options);
+
+    rectangle.setMap(null);
+    rectPoly.setMap(map);
+    return rectPoly;
+}
+
+
+function getCoordinates() {
+  var contentString;
+  var vertices = rectPoly.getPath();
+
+  // Iterate over the vertices.
+  for (var i =0; i < vertices.getLength(); i++) {
+       var xy = vertices.getAt(i);
+          contentString += '<br>' + 'Coordinate ' + i + ':<br>' + xy.lat() + ',' +
+              xy.lng();
+        }
+  return contentString;
+
 }
