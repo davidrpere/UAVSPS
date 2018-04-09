a9b12e6a5dce486d4b2fa5ac9f99fa0d7497fe42
drodriguez <mxmax95@gmail.com>
2018-04-09 19:58:53 +0200

    Commit mavros

---
+++ controller_node/mavros/ros/kinetic/include/mavlink/config.h
@@ -0,0 +1,1 @@
+#define MAVLINK_VERSION "2017.9.9"
---
+++ controller_node/mavros/ros/kinetic/share/gennodejs/ros/geographic_msgs/msg/_index.js
@@ -0,0 +1,34 @@
+
+"use strict";
+
+let GeoPath = require('./GeoPath.js');
+let WayPoint = require('./WayPoint.js');
+let GeographicMapChanges = require('./GeographicMapChanges.js');
+let MapFeature = require('./MapFeature.js');
+let RoutePath = require('./RoutePath.js');
+let GeoPoseStamped = require('./GeoPoseStamped.js');
+let BoundingBox = require('./BoundingBox.js');
+let GeoPointStamped = require('./GeoPointStamped.js');
+let RouteNetwork = require('./RouteNetwork.js');
+let GeoPoint = require('./GeoPoint.js');
+let RouteSegment = require('./RouteSegment.js');
+let GeographicMap = require('./GeographicMap.js');
+let KeyValue = require('./KeyValue.js');
+let GeoPose = require('./GeoPose.js');
+
+module.exports = {
+  GeoPath: GeoPath,
+  WayPoint: WayPoint,
+  GeographicMapChanges: GeographicMapChanges,
+  MapFeature: MapFeature,
+  RoutePath: RoutePath,
+  GeoPoseStamped: GeoPoseStamped,
+  BoundingBox: BoundingBox,
+  GeoPointStamped: GeoPointStamped,
+  RouteNetwork: RouteNetwork,
+  GeoPoint: GeoPoint,
+  RouteSegment: RouteSegment,
+  GeographicMap: GeographicMap,
+  KeyValue: KeyValue,
+  GeoPose: GeoPose,
+};
