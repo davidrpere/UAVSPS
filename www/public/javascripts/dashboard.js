633067f85ef656ca12223e5bb4e08e79abb0a1bb
Ruben Perez Vaz <rubenperezvaz@MacBook-Pro-de-Ruben.local>
2018-04-09 13:01:59 +0200

    configure mission done

--- www/public/javascripts/dashboard.js
+++ www/public/javascripts/dashboard.js
@@ -1,12 +1,19 @@
 var rectPoly;
-var mapa;
+var map;
 var params;
+
+var div_google_maps = new Vue({
+    el: '#div_google_maps',
+    data: {
+        seen: true
+    }
+});
 
 var btnContinue = new Vue({
     el: '#btnContinue',
     methods: {
-        getData: function() {
-            if (!$("#nombre_mision").val()) {
+        getData: function () {
+            if (!$("#mission_name").val()) {
                 alert_name_mision.seen = true
             } else {
                 params = $("form").serializeArray()
@@ -16,23 +23,23 @@ var btnContinue = new Vue({
             }
         }
     }
-})
+});
 
 var btnClose = new Vue({
     el: "#btnClose",
     methods: {
-        hide_alert_name_mision: function() {
+        hide_alert_name_mision: function () {
             alert_name_mision.seen = false
         }
     }
-})
+});
 
 var btnSetMission = new Vue({
     el: "#btnSetMission",
     data: {
         seen: true
     }
-})
+});
 
 var btnArea = new Vue({
     el: "#btnArea",
@@ -40,95 +47,63 @@ var btnArea = new Vue({
         seen: false
     },
     methods: {
-        add_area: function() {
+        add_area: function () {
             add_area();
         },
-        remove_area: function() {
+        remove_area: function () {
             remove_area();
         },
-        start_mission: function() {
-            var coordinates = getCoordinates()
+        start_mission: function () {
+            var array_coordJSON = [];
 
-            console.log(coordinates);
-            console.log(params)
+            var MVCArray = rectPoly.getPath();
+
+            MVCArray.forEach(function (element) {
+                array_coordJSON.push(element.toJSON());
+            });
 
             var socket = io();
-            socket.emit('empezar_mision', { my: 'data1'});
-        }
-    }
-})
+            socket.emit('start_mission', {
+                mission_name: params[0].value,
+                mission_type: params[1].value,
+                altitude_fly: params[2].value,
+                number_drones: params[3].value,
+                overlap: params[4].value,
+                north_east: array_coordJSON[0],
+                north_west: array_coordJSON[1],
+                south_east: array_coordJSON[3],
+                south_west: array_coordJSON[2]
+            });
 
-var name_mission = new Vue({
-    el: "#nombre_mision",
-    methods: {
-        hide_alert_name_mision: function() {
-            alert_name_mision.seen = false
+            div_google_maps.seen = false;
+            btnArea.seen = false;
+            progress_bar.seen = true;
         }
     }
-})
+});
+
 
 var alert_name_mision = new Vue({
     el: '#alert_name_mision',
     data: {
         seen: false
     }
-})
+});
+
+var progress_bar = new Vue({
+    el: '#progress_bar',
+    data: {
+        seen: false
+    }
+});
 
 /**
- * Google Maps
+ * Google Maps initMap function
  */
-function initMap() {
-    /*var map;
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
 
-        map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 18,
-          center: {
-            lat: 42.1694839,
-            lng: -8.683478499999978
-          },
-          mapTypeId: 'satellite',
-          mapTypeControl: false,
-          scaleControl: true,
-          fullscreenControl: false
-        });
-
-        map.setCenter(pos);
-      }, function() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 18,
-          center: {
-            lat: 42.1694839,
-            lng: -8.683478499999978
-          },
-          mapTypeId: 'satellite',
-          mapTypeControl: false,
-          scaleControl: true,
-          fullscreenControl: false
-        });
-      });
-    } else {
-      // Browser doesn't support Geolocation
-      map = new google.maps.Map(document.getElementById('map'), {
+function initAutocomplete() {
+    map = new google.maps.Map(document.getElementById('map'), {
         zoom: 18,
-        center: {
-          lat: 42.1694839,
-          lng: -8.683478499999978
-        },
-        mapTypeId: 'satellite',
-        mapTypeControl: false,
-        scaleControl: true,
-        fullscreenControl: false
-      });
-    }*/
-    mapa = new google.maps.Map(document.getElementById('map'), {
-        zoom: 16,
         center: {
             lat: 42.1694839,
             lng: -8.683478499999978
@@ -137,18 +112,137 @@ function initMap() {
         mapTypeControl: false,
         scaleControl: true,
         fullscreenControl: false,
-        heading: 90,
-        tilt: 45
+        disableDoubleClickZoom: false,
+        gestureHandling: "auto"
+
+    });
+
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function () {
+        searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function () {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        // Clear out the old markers.
+        markers.forEach(function (marker) {
+            marker.setMap(null);
+        });
+        markers = [];
+
+        // For each place, get name and location.
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function (place) {
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+                map: map,
+                title: place.name,
+                position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        map.fitBounds(bounds);
     });
 }
 
+/*function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 17,
+        center: {
+            lat: 42.1694839,
+            lng: -8.683478499999978
+        },
+        mapTypeId: 'satellite',
+        mapTypeControl: false,
+        scaleControl: true,
+        fullscreenControl: false,
+        disableDoubleClickZoom: false,
+        gestureHandling: "auto"
+
+    });
+
+    /* Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 17,
+                center: {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                },
+                mapTypeId: 'satellite',
+                mapTypeControl: false,
+                scaleControl: true,
+                fullscreenControl: false
+            });
+        }, function () {
+            map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 17,
+                center: {
+                    lat: 42.1694839,
+                    lng: -8.683478499999978
+                },
+                mapTypeId: 'satellite',
+                mapTypeControl: false,
+                scaleControl: true,
+                fullscreenControl: false
+            });
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 17,
+            center: {
+                lat: 42.1694839,
+                lng: -8.683478499999978
+            },
+            mapTypeId: 'satellite',
+            mapTypeControl: false,
+            scaleControl: true,
+            fullscreenControl: false
+        });
+    }
+}*/
+
 function add_area() {
     //Define the LatLng coordinates for the rectangle.
+    var jsonMapBounds = map.getBounds().toJSON();
+    var jsonMapCenter = map.getCenter().toJSON();
+
+    var north = midpoint_coordinates(jsonMapBounds.north, jsonMapCenter.lng, jsonMapCenter.lat, jsonMapCenter.lng).lat
+    var south = midpoint_coordinates(jsonMapCenter.lat, jsonMapCenter.lng, jsonMapBounds.south, jsonMapCenter.lng).lat
+    var east = midpoint_coordinates(jsonMapCenter.lat, jsonMapCenter.lng, jsonMapCenter.lat, jsonMapBounds.east).lon
+    var west = midpoint_coordinates(jsonMapCenter.lat, jsonMapCenter.lng, jsonMapCenter.lat, jsonMapBounds.west).lon
+
     var bounds = {
-        north: 42.1694839,
-        south: 42.1634839,
-        east: -8.683478499999978,
-        west: -8.689478499999978
+        north: north,
+        south: south,
+        east: east,
+        west: west
     };
 
     // Construct the polygon.
@@ -159,7 +253,7 @@ function add_area() {
         strokeWeight: 2,
         fillColor: '#FF0000',
         fillOpacity: 0.35,
-        map: mapa,
+        map: map,
         editable: true,
         draggable: true
     });
@@ -167,30 +261,20 @@ function add_area() {
     rectPoly = createPolygonFromRectangle(rectangle);
 }
 
-function remove_area() {
-    rectPoly.setMap(null)
-}
-
 function createPolygonFromRectangle(rectangle) {
-    var map = rectangle.getMap();
-
     var coords = [{
         lat: rectangle.getBounds().getNorthEast().lat(),
         lng: rectangle.getBounds().getNorthEast().lng()
-    },
-        {
-            lat: rectangle.getBounds().getNorthEast().lat(),
-            lng: rectangle.getBounds().getSouthWest().lng()
-        },
-        {
-            lat: rectangle.getBounds().getSouthWest().lat(),
-            lng: rectangle.getBounds().getSouthWest().lng()
-        },
-        {
-            lat: rectangle.getBounds().getSouthWest().lat(),
-            lng: rectangle.getBounds().getNorthEast().lng()
-        }
-    ];
+    }, {
+        lat: rectangle.getBounds().getNorthEast().lat(),
+        lng: rectangle.getBounds().getSouthWest().lng()
+    }, {
+        lat: rectangle.getBounds().getSouthWest().lat(),
+        lng: rectangle.getBounds().getSouthWest().lng()
+    }, {
+        lat: rectangle.getBounds().getSouthWest().lat(),
+        lng: rectangle.getBounds().getNorthEast().lng()
+    }];
 
     // Construct the polygon.
     var rectPoly = new google.maps.Polygon({
@@ -198,31 +282,44 @@ function createPolygonFromRectangle(rectangle) {
         editable: true,
         draggable: true
     });
+
     var properties = ["strokeColor", "strokeOpacity", "strokeWeight", "fillOpacity", "fillColor"];
+
     //inherit rectangle properties
     var options = {};
-    properties.forEach(function(property) {
+    properties.forEach(function (property) {
         if (rectangle.hasOwnProperty(property)) {
             options[property] = rectangle[property];
         }
     });
     rectPoly.setOptions(options);
 
-    rectangle.setMap(null);
-    rectPoly.setMap(map);
+    rectangle.setMap(null); //Delete rectangle.
+    rectPoly.setMap(map); //Add polygon with rectangle form.
     return rectPoly;
 }
 
-function getCoordinates() {
-    var contentString;
-    var vertices = rectPoly.getPath();
+function remove_area() {
+    rectPoly.setMap(null)
+}
 
-    // Iterate over the vertices.
-    for (var i = 0; i < vertices.getLength(); i++) {
-        var xy = vertices.getAt(i);
-        contentString += '<br>' + 'Coordinate ' + i + ':<br>' + xy.lat() + ',' +
-            xy.lng();
-    }
-    return contentString;
+function midpoint_coordinates(lat1, lon1, lat2, lon2) {
+    var dLon = (lon2 - lon1) * (Math.PI / 180);
 
+    //convert to rad
+    lat1 = lat1 * (Math.PI / 180);
+    lat2 = lat2 * (Math.PI / 180);
+    lon1 = lon1 * (Math.PI / 180);
+
+    var Bx = Math.cos(lat2) * Math.cos(dLon);
+    var By = Math.cos(lat2) * Math.sin(dLon);
+
+    var lat3 = Math.atan2(Math.sin(lat1) + Math.sin(lat2), Math.sqrt((Math.cos(lat1) + Bx) * (Math.cos(lat1) + Bx) + By * By));
+    var lon3 = lon1 + Math.atan2(By, Math.cos(lat1) + Bx);
+
+    //Convert to degrees
+    lat3 = lat3 * (180 / Math.PI);
+    lon3 = lon3 * (180 / Math.PI);
+
+    return {"lat": lat3, "lon": lon3}
 }
